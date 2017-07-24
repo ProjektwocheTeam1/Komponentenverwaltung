394d58715fd1dbf733b5e5e4254fa49077ce7c50
Felix <felixbinder123@gmx.de>
2017-07-24 11:49:03 +0200

    Übersicht aller Komponenten

---
+++ generalOverview.php
@@ -0,0 +1,13 @@
+<div id="generalOverview">
+  <form action="create.php" method="post">
+    <h3 id="gOverviewHeader">Übersicht aller Komponenten</h3>
+
+    <input type="submit" name="btnAnlegen" value="Anlegen">
+    <table class="sortable">
+      <tr> </tr>
+      <?php
+        // Komponenten anzeigen
+      ?>
+    </table>    
+  </form>
+</div>
