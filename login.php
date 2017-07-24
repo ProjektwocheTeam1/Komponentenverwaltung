f5cfea49f7c5812bdbf5c55905e574a9309e88d3
LukasDallhammer <lukas.ld.dallhammer@web.de>
2017-07-24 11:24:35 +0200

    Login and Overview
    Login and Overview as dummies.

---
+++ login.php
@@ -0,0 +1,19 @@
+<html>
+ <head>
+	<meta charset="utf-8">
+	<title>Login: IT-Verwaltung</title>
+ </head>
+ <body>
+	<form method="POST" action="overview.php">
+		<label for="BName">Benutzername:
+			<input type ="text" id="BName" name="Benutzername">
+		</label>
+		<br />
+		<label for="Pass">Passwort:
+			<input type ="password" id="Pass" name="Passwort">
+		</label>
+		<br />
+		<input type="submit" name="login" value="Anmelden">
+	</form>
+ </body>
+</html>
---
+++ overview.php
@@ -0,0 +1,29 @@
+<html>
+ <head>
+	<meta charset="utf-8">
+	<title>Übersicht: IT-Verwaltung</title>
+ </head>
+ <body>
+	<div>
+		<img src="http://www.b3-fuerth.de/b3/home.nsf/imgref/Image_logo_metall.png/$FILE/logo_metall.png">
+		<!-- Nav-Leiste -->
+	</div>
+	<div>
+		<form method="POST" action="overview.php">
+			<label for="KSuche">Komponente suchen:
+				<input type ="text" id="KSuche" name="components">
+			</label>
+			<input class="hidden" type="submit" value="KSuche">
+		</form>
+		<?php 
+			foreach($rooms as $room)
+			{
+				?>
+				<div>
+					Raum 1
+					<!-- echo Room Name -->
+				</div>
+				<?php
+			}
+		?>
+	</div>
