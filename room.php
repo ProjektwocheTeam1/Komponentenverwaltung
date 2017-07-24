8031091753031069df8a55a368833af44ee1da48
LukasDallhammer <lukas.ld.dallhammer@web.de>
2017-07-24 13:11:31 +0200

    Room.php

---
+++ room.php
@@ -0,0 +1,14 @@
+<html>
+ <head>
+	<meta charset="utf-8">
+	<title><?php echo $title; ?></title>
+ </head>
+ <body>
+	<?php include('assets/nav.php'); ?>
+	<h1><?php echo $room; ?></h1>
+		<div>
+		<form method="GET" action="create.php? type=component">
+		<input type="submit" value="Komponente anlegen" >
+	<table>	
+ </body>
+</html>			
