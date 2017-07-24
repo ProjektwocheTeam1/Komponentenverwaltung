6833c68ef50ea00ead2f80c7f8e89ba8f6ce6f52
Felix <felixbinder123@gmx.de>
2017-07-24 13:28:05 +0200

    Übersichtstabelle angepasst

--- generalOverview.php
+++ generalOverview.php
@@ -1,8 +1,23 @@
+<?php
+  $title = "Komponentenübersicht";
+	$result = array(
+		"Beschreibung" => "Test Beschreibung",
+		"Raum" => "123",
+		"Einkaufsdatum" => "01.01.2000",
+		"Gewährleistungsdauer" => "365",
+		"Notiz" => "Test Notiz",
+		"Hersteller" => "Test Hersteller",
+		"Komponentenart" => "Test Komponentenart",
+		"Lieferant" => "Test Lieferant"
+	);
+	// Get DB array here as $result!
+?>
+
 <html>
   <head>
     <meta charset="utf-8">
   	<title><?php echo $title; ?></title>
-    <script src="Assets/sorttable.js" charset="utf-8"></script>
+    <script src="assets/sorttable.js" charset="utf-8"></script>
   </head>
 
   <body>
@@ -11,11 +26,30 @@
         <h3 id="gOverviewHeader">Übersicht aller Komponenten</h3>
 
         <input type="submit" name="btnAnlegen" value="Anlegen">
-        <table class="sortable">
-        <tr> </tr>
+        <table class="sortable" border="1">
+          <tr>
           <?php
-          // Komponenten anzeigen
-          ?>
+            // Generate Table-Header
+      			foreach($result as $key => $value)
+      			{
+      				?>
+      					<th><?php echo $key; ?></th>
+      				<?php
+      			}
+      		?>
+          </tr>
+
+          <tr>
+          <?php
+            // Generate Table-Data
+      			foreach($result as $key => $value)
+      			{
+      				?>
+      					<td><?php echo $value; ?></td>
+      				<?php
+      			}
+      		?>
+        </tr>
         </table>
       </form>
     </div>
