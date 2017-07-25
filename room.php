e2aceb2af22709f04ca39e90b6383684419f5caf
LukasDallhammer <lukas.ld.dallhammer@web.de>
2017-07-25 14:55:57 +0200

    braedcrumb by taner
    Breadcrumb Navigation as function.

--- room.php
+++ room.php
@@ -7,10 +7,17 @@
  
  <body>
 	<?php include('assets/nav.php'); ?>
-	<h1><?php echo $room; ?></h1>
-		<div>
+	<div>
+		<a href="overview.php">Start</a> >
+		<a href="generalOverview.php?type=raum">Räume</a> >
+		<a href="#">Raum <?= $_GET['room']?></a>
+	</div>
+	<div class="content">
+		<h1><?php echo $room; ?></h1>
 		<form method="GET" action="create.php?type=component">
-		<input type="submit" value="Komponente anlegen" >
-	<table>	
+			<input type="submit" value="Komponente anlegen" >
+		</form>
+		<?php include('assets/table.php'); ?>
+	</div>
  </body>
 </html>			
