534ff3938298213e02edeb8eb822d30d42c9c216
Felix <felixbinder123@gmx.de>
2017-07-25 09:24:20 +0200

    Added update-, config- and delete-button

--- generalOverview.php
+++ generalOverview.php
@@ -34,43 +34,58 @@
 
   <body>
     <div id="generalOverview">
+      <h3 id="gOverviewHeader">Übersicht aller Komponenten</h3>
+
       <form action="create.php" method="post">
-        <h3 id="gOverviewHeader">Übersicht aller Komponenten</h3>
-
         <input type="submit" name="btnAnlegen" value="Anlegen">
-
-        <?php if (count($result) > 0) { ?>
-        <table class="sortable" border="1">
-        <tr>
-        <?php
-          // Generate Table-Header
-          foreach($result[0] as $key => $value) {
-            ?>
-            <th><?php echo $key; ?></th>
-            <?php
-          }
-        ?>
-        </tr>
-
-        <?php
-    		  for($i = 0; $i < count($result); $i++) {
-    			  ?>
-    			  <tr>
-      			  <?php
-              // Generate Table-Data
-      			  foreach($result[$i] as $key => $value) {
-        			  ?>
-        				<td><?php echo $value; ?></td>
-        			  <?php
-      			  }
-      			  ?>
-      			</tr>
-      			<?php
-    		  }
-  		  ?>
-        </table>
-        <?php } ?>
       </form>
+
+      <?php if (count($result) > 0) { ?>
+      <table class="sortable" border="1">
+      <tr>
+      <?php
+        // Generate Table-Header
+        foreach($result[0] as $key => $value) {
+          ?>
+          <th><?php echo $key; ?></th>
+          <?php
+        }
+      ?>
+      </tr>
+
+      <?php
+  		  for($i = 0; $i < count($result); $i++) {
+  			  ?>
+  			  <tr>
+    			  <?php
+            // Generate Table-Data
+    			  foreach($result[$i] as $key => $value) {
+      			  ?>
+      				<td><?php echo $value; ?></td>
+      			  <?php
+    			  }
+    			  ?>
+  				  <td>
+              <form action="create.php" method="post">
+                <input type="submit" name="btnCopy" value="Kopieren">
+              </form>
+  				  </td>
+  				  <td>
+              <form action="update.php" method="post">
+                <input type="submit" name="btnConfig" value="Konfigurieren">
+              </form>
+  				  </td>
+  				  <td>
+              <form action="delete.php" method="post">
+                <input type="submit" name="btnDelete" value="Löschen">
+              </form>
+  				  </td>
+    			</tr>
+    			<?php
+  		  }
+		  ?>
+      </table>
+      <?php } ?>
     </div>
   </body>
 </html>
