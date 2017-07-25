9c4271e901e18ca475541d38b16fb372f3163c76
Felix <felixbinder123@gmx.de>
2017-07-25 09:33:12 +0200

    Overview-table outsourced.

--- generalOverview.php
+++ generalOverview.php
@@ -40,52 +40,15 @@
         <input type="submit" name="btnAnlegen" value="Anlegen">
       </form>
 
-      <?php if (count($result) > 0) { ?>
-      <table class="sortable" border="1">
-      <tr>
-      <?php
-        // Generate Table-Header
-        foreach($result[0] as $key => $value) {
-          ?>
-          <th><?php echo $key; ?></th>
-          <?php
-        }
-      ?>
-      </tr>
-
-      <?php
-  		  for($i = 0; $i < count($result); $i++) {
-  			  ?>
-  			  <tr>
-    			  <?php
-            // Generate Table-Data
-    			  foreach($result[$i] as $key => $value) {
-      			  ?>
-      				<td><?php echo $value; ?></td>
-      			  <?php
-    			  }
-    			  ?>
-  				  <td>
-              <form action="create.php" method="post">
-                <input type="submit" name="btnCopy" value="Kopieren">
-              </form>
-  				  </td>
-  				  <td>
-              <form action="update.php" method="post">
-                <input type="submit" name="btnConfig" value="Konfigurieren">
-              </form>
-  				  </td>
-  				  <td>
-              <form action="delete.php" method="post">
-                <input type="submit" name="btnDelete" value="Löschen">
-              </form>
-  				  </td>
-    			</tr>
-    			<?php
-  		  }
-		  ?>
-      </table>
-      <?php } ?>
+      <?php if (count($result) > 0) {
+        include('assets/table.php');
+      } else {
+        ?>
+        <div>
+          Keine Komponenten vorhanden!
+        </div>
+        <?php
+      } ?>
     </div>
   </body>
 </html>
