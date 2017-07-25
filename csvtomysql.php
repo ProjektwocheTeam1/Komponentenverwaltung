815d83d2057bf48bfd5f4e02d3f8a7545e682a4e
MarcelKloss <marcelkloss1@gmx.de>
2017-07-25 14:58:38 +0200

    csvtomysql

--- csvtomysql.php
+++ csvtomysql.php
@@ -1,28 +1,26 @@
 <?php
+
+include("Assets/helpers.php");
+
+redirectToLogin();
+establishLinkForUser();
+//CSV in Array einlesen
 $fp = fopen("E:\userlist.csv", "r");
 $zeilen = array();
-
 while( !feof($fp) ) {
     $zeilen[] = fgetcsv  ( $fp  , 4096 , ";" , "\"" );
 }
 fclose($fp);
 
+//Passwort verschlüsseln
 foreach ($zeilen as &$value){
 	$value[1] = password_hash($value[1], 1);
 	}
-?>
 
-<br><br><br><br>
-
-<?php
+//INSERT Array to mysql-db
 $con = mysqli_connect("localhost", "testuser", "test", "usertest");
 foreach ($zeilen as &$value){
-	echo "<br><br>";
-	echo "<br><br>";
 	$sql = "INSERT INTO benutzer (username, passwort, rechte_id, benutzervorname, benutzernachname) VALUES ('".$value[0]."', '".$value[1]."', ".$value[2].", '".$value[3]."', '".$value[4]."');";
-
-	var_dump($sql);
-	//var_dump(mysqli_query($con, $sql));
 	mysqli_query($con, $sql);
 }
 ?>
