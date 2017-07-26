b3c6cb9545dc42bb63736961e76b841bfebbacea
Dominik <dominik.berger@silbury.com>
2017-07-26 15:23:47 +0200

    Create

--- create.php
+++ create.php
@@ -1,26 +1,52 @@
 <?php
 include("Assets/helpers.php");
-	$con = establishLinkForUser();
+	redirectToLogin();
+	$Con = establishLinkForUser();
 	if(isset($_GET['type']))
 	{
-		if(){
-
+		if($_GET['type']=='raeume'){
+			$result =array(
+				"RaumNr",
+				"Bezeichnung",
+				"Notiz"
+			);
+		}elseif ($_GET['type']=="lieferant"){
+			$result =array(
+				"Firmenname",
+				"Strasse",
+				"Postleitzahl",
+				"Ort",
+				"Tel.",
+				"Fax",
+				"Email"
+			);
+		}elseif($_GET['type']=="komponentenarten"){
+			$result = array("Komponentenart",);
+		}
+		elseif($_GET['type']=="komponentenattribute"){
+			$result = array("Bezeichnung");
+		}
+		elseif($_GET['type']=="komponenten"){
+			$result = array("");
 		}
 	}
 	// Get DB array here as $result!
 ?>
 
+<style>
+.delete { margin-left: 6cm;}
 
+</style>
 <html>
 	<?php include("Assets/header.php");?>
 	<body>
-		<?php redirectToLogin(); include("Assets/nav.php");?>
-		<div>
+		<?php include("Assets/nav.php");?>
+		<div class="delete">
 			<h2>Anlegen</h2>
 			<form method="post" action="overview.php">
 				<table>
 					<?php
-					foreach($result as $key => $value)
+					foreach($result as $key)
 					{
 						?>
 						<tr>
