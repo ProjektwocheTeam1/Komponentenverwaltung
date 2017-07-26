55d3fc942d4a0d9600ea0c4198686c8dfc8bf768
TanerD <taner96@web.de>
2017-07-26 14:45:53 +0200

    bugfix

--- Assets/nav.php
+++ Assets/nav.php
@@ -1,15 +1,15 @@
 <div class="navbar-left">
 	<img src="assets/img/logo.png" class="img_logo"/>
 	<nav role="main">
-		<a href="overview.php">Start</a>
-		<span>Verwaltung</span>
+		<a class="nav_a" href="overview.php">Start</a>
+		<span class="nav_span" >Verwaltung</span>
 		<ul>
-			<li><a href="./supplierOverview.php">Lieferanten</a></li>
-			<li><a href="./roomOverview.php">R&auml;ume</a></li>
-			<li><a href="./userOverview.php">Benutzer</a></li>
-			<li><a href="./componentOverview.php">Komponenten</a></li>
-			<li><a href="./compKindOverview.php">Komponentenarten</a></li>
-			<li><a href="./compAttrOverview.php">Komponentenattribute</a></li>
+			<li><a class="nav_a" href="./supplierOverview.php">Lieferanten</a></li>
+			<li><a class="nav_a" href="./roomOverview.php">R&auml;ume</a></li>
+			<li><a class="nav_a" href="./userOverview.php">Benutzer</a></li>
+			<li><a class="nav_a" href="./componentOverview.php">Komponenten</a></li>
+			<li><a class="nav_a" href="./compKindOverview.php">Komponentenarten</a></li>
+			<li><a class="nav_a" href="./compAttrOverview.php">Komponentenattribute</a></li>
 		</ul>
 	</nav>
 </div>
