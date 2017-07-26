d28a0402918cb5e61bf099ea73f011a7375fed98
Felix <felixbinder123@gmx.de>
2017-07-26 10:02:23 +0200

    Fixed typos

--- Assets/helpers.php
+++ Assets/helpers.php
@@ -6,7 +6,7 @@
 function redirectToLogin()
 {
 	session_start();
-	
+
 	if(empty($_SESSION['user']))
 	{
 		header('Location: login.php');
@@ -37,13 +37,13 @@ function breadCrumb()
 	{
 		$_SESSION['History'] = array();
 	}
-	
+
 	$url = "http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
 	$urlArray = explode("/", $url);
 	$lastEl = array_values(array_slice($urlArray, -1))[0];
 	$split = explode(".", $lastEl);
 	$currentPage = $split[0];
-	
+
 	if(in_array($currentPage, $_SESSION['History']))
 	{
 		$key = array_search($currentPage, $_SESSION['History']);
@@ -53,14 +53,14 @@ function breadCrumb()
 	{
 		$_SESSION['History'][] = $currentPage;
 	}
-	
+
 	$breadCrumb = '<div>';
 	foreach($_SESSION['History'] as $value)
 	{
-		$breadCrumb.= '<a href="'.$value'.php">'.$value.'</a> > ';
+		$breadCrumb.= '<a href="'.$value.'.php">'.$value.'</a> > ';
 	}
 	$breadCrumb.= '</div>';
-	
+
 	return $breadCrumb;
 }
 ?>
