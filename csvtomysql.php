721cccf9f21163c3423339a023a9d073c0c107bf
Lukas Dallhammer <lukas.ld.dallhammer@web.de>
2017-07-26 09:38:46 +0200

    Deployement version userlist import. Lukas

--- csvtomysql.php
+++ csvtomysql.php
@@ -5,7 +5,7 @@ include("Assets/helpers.php");
 redirectToLogin();
 establishLinkForUser();
 //CSV in Array einlesen
-$fp = fopen("E:\userlist.csv", "r");
+$fp = fopen("Assets/userlist.csv", "r");
 $zeilen = array();
 while( !feof($fp) ) {
     $zeilen[] = fgetcsv  ( $fp  , 4096 , ";" , "\"" );
@@ -20,7 +20,8 @@ foreach ($zeilen as &$value){
 //INSERT Array to mysql-db
 $con = mysqli_connect("localhost", "testuser", "test", "usertest");
 foreach ($zeilen as &$value){
-	$sql = "INSERT INTO benutzer (username, passwort, rechte_id, benutzervorname, benutzernachname) VALUES ('".$value[0]."', '".$value[1]."', ".$value[2].", '".$value[3]."', '".$value[4]."');";
+	$sql = "INSERT INTO benutzer (username, passwort, rechte_id, benutzervorname, benutzernachname)
+	VALUES ('".$value[0]."', '".$value[1]."', ".$value[2].", '".$value[3]."', '".$value[4]."');";
 	mysqli_query($con, $sql);
 }
 ?>
