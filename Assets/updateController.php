9be343a724aa3642614d401dcacbced388eed253
LukasDallhammer <lukas.ld.dallhammer@web.de>
2017-07-27 15:36:01 +0200

    interface fix on update

--- Assets/updateController.php
+++ Assets/updateController.php
@@ -7,7 +7,6 @@
 **/
 	if(isset($_POST['update_btn']))
 	{
-		var_dump($_POST);
 		switch ($_POST['controller']) {
 			case 'Komponentenattribut':
 				$updateDatabaseSQL = "
@@ -44,7 +43,7 @@
 					k_gewaehrleistungsdauer = {$_POST['Gewährleistung_(in_Jahren)']},
 					k_notiz = '{$_POST['Notiz']}',
 					k_hersteller = '{$_POST['Hersteller']}',
-					komponentenart_ka_id = {$_POST['Komponentenart']},
+					komponentenarten_ka_id = {$_POST['Komponentenart']},
 					log_id = $log
 				WHERE k_id = {$_POST['id']};";
 				break;
@@ -78,7 +77,6 @@
 				break;
 		}
 		//n-m-Tabellen Pflegen
-		var_dump($updateDatabaseSQL);
 		mysqli_query($con, $updateDatabaseSQL);
 	}
 ?>
