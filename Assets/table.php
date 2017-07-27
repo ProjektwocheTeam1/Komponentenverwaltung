84a9d8256c4ad0a2abc40f99a9ea24c21dad422f
TanerD <taner96@web.de>
2017-07-27 11:34:57 +0200

    icon btn changes

--- Assets/table.php
+++ Assets/table.php
@@ -50,7 +50,7 @@
           <form action="update.php" method="post">
             <input type="hidden" name="type" value="<?php echo $type; ?>">
             <input type="hidden" name="id" value="<?php echo $v['ID']; ?>">
-            <div class="iconDiv"><img src="Assets/img/Pencil.png" class="tableIcon" onclick="submit();"/></div>
+            <button type="submit" class="iconBtn"><img src="Assets/img/Pencil.png" class="tableIcon"/></button>
 			<!--<input type="submit" name="btnConfig" value="Konfigurieren">-->
           </form>
         </td>
@@ -58,7 +58,7 @@
           <form action="delete.php" method="post">
             <input type="hidden" name="type" value="<?php echo $type ?>">
             <input type="hidden" name="id" value="<?php echo $v['ID']; ?>">
-            <div class="iconDiv"><img src="Assets/img/Delete.png" class="tableIcon" onclick="submit();"/></div>
+            <button type="submit" class="iconBtn"><img src="Assets/img/Delete.png" class="tableIcon"/></button>
             <!--<input type="submit" name="btnDelete" value="Löschen">-->
           </form>
         </td>
