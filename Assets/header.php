43561120575df26a54489b86bdfd7ebcd374cce2
TanerD <taner96@web.de>
2017-07-27 11:46:39 +0200

    jquery added with datepicker

--- Assets/header.php
+++ Assets/header.php
@@ -2,6 +2,14 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=Edge">
 	<meta charset="utf-8">
 	<link rel="stylesheet" type="text/css" href="Assets/styling.css">
+	<link rel="stylesheet" href="Assets/JQuery/jquery-ui-1.12.1.custom/jquery-ui.theme.min.css">
 	<script src="Assets/sorttable.js"></script>
+	<script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+	<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+	<script>
+		$(function() {
+			$( "#datepicker" ).datepicker();
+		});
+	</script>
 	<title><?php echo $title; ?></title>
 </head>
